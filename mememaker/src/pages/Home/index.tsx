4dce54146ebdc2eced452a93e53e5fa135d6c27d
brunomatheusc <bmatheusc@gmail.com>
2020-06-10 23:42:29 -0300

    Adding mememaker

---
+++ mememaker/src/pages/Home/index.tsx
@@ -0,0 +1,120 @@
+import React, { FC, useState, useEffect, ChangeEvent, FormEvent } from 'react';
+import { Wrapper, Card, Templates, Form, Button, Meme } from './styles';
+
+import qs from 'qs';
+
+import logo from '../../assets/logo.svg';
+import api from '../../services/api';
+
+interface IProps {
+    data: IData;
+}
+
+interface IData {
+    memes: ITemplate[];
+}
+
+interface ITemplate {
+    id: number;
+    name: string;
+    url: string;
+    box_count: number;
+}
+
+const Home: FC = () => {
+    const [templates, setTemplates] = useState<ITemplate[]>([]);
+    const [selectedTemplate, setSelectedTemplate] = useState<ITemplate>();
+    const [boxes, setBoxes] = useState<string[]>([]);
+    const [generatedMeme, setGeneratedMeme] = useState<string>('');
+
+    useEffect(() => {
+        (async () => {
+            const response = await api.get('/get_memes');           
+            const { data: { memes } } = response.data;
+
+            setTemplates(memes);
+        })();
+    }, []);
+
+    const handleInputChange = (index: number) => (e: ChangeEvent<HTMLInputElement>) => {
+        const newValues = boxes;
+        newValues[index] = e.target.value;
+        setBoxes(newValues);
+    }
+
+    function handleSelectTemplate(template: ITemplate) {
+        setSelectedTemplate(template);
+        setBoxes([]);
+    }
+
+    function handleReset() {
+        setSelectedTemplate(undefined);
+        setBoxes([]);
+        setGeneratedMeme('');
+    }
+
+    async function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+
+        const params = qs.stringify({
+            template_id: selectedTemplate?.id,
+            username: 'vikayel543',
+            password: 'vikayel543',
+            boxes: boxes.map(text => ({ text })),
+        });
+
+        const response = await api.get(`/caption_image?${params}`);
+        const { data: { url }} = response.data;
+
+        console.log(response);
+
+        setGeneratedMeme(url);
+    }
+
+    return (
+        <>
+            <Wrapper>
+                <img src={ logo } alt="MemeMaker"/>
+
+                <Card>
+                    { generatedMeme && (
+                        <>
+                            <Meme src={ generatedMeme } alt="Generated Meme"/>
+                            <Button onClick={ handleReset }>Criar outro meme</Button>
+                        </>
+                    )}
+                    { !generatedMeme && (
+                        <>
+                            <h2>Selecione um template</h2>
+        
+                            <Templates>
+                                { templates.map((template) => (
+                                    <button key={ template.id } onClick={ () => handleSelectTemplate(template) } className={ template.id == selectedTemplate?.id ? 'selected' : '' }>
+                                        <img src={ template.url } alt={ template.name } />
+                                    </button>
+                                ))}
+                            </Templates>
+        
+                            { selectedTemplate && (
+                                <>
+                                    <h2>Textos</h2>
+        
+                                    <Form onSubmit={ handleSubmit }>
+                                        { (new Array(selectedTemplate.box_count).fill('').map((_, index) => (
+                                            <input type="text" key={index} placeholder={ `Text #${index + 1}`} onChange={ handleInputChange(index) } />
+                                        )))}
+        
+                                        <Button type="submit">MakeMyMeme</Button>
+                                    </Form>
+                                </>
+                            )}
+                        </>
+                    )}
+
+                </Card>
+            </Wrapper>
+        </>
+    );
+};
+
+export default Home;
